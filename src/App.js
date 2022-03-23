104bdc9181a11830467a632ae80f5591dc8f6eef
StormKing969 <wijesinghesaj@gmail.com>
2022-03-23 12:51:29 -0400

    auto-generation of user input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Header from "./components/Header"
 import Note from "./components/Note"
 import Footer from "./components/Footer"
+import notes from "./notes";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      
+      <div className="note_section">
+        {notes.map(notes => (
+          <Note 
+            key = {notes.key}
+            title={notes.title}
+            content={notes.content}
+          />
+        ))}
+      </div>
+
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
-function Note() {
+function Note(props) {
     return (
         <div className="note">
             <h1 className="note_title">
-                This is the note title
+                {props.title}
             </h1>
             <p className="note_content">
-                This is a note templete
+                {props.content}
             </p>
         </div>
     );
